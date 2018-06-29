9d3bfaa3119c38020690f786c45f389da2c36f88
Crablicious <adwe@live.se>
2018-06-29 09:07:08 -0400

    runscript update

--- run_rfc2544.sh
+++ run_rfc2544.sh
@@ -1,3 +1,2 @@
-# sudo ./build/MoonGen rfc2544/master.lua --rxport 0 --txport 1 --iterations 1 --duration 1
-sudo ./build/MoonGen rfc2544/master.lua --rxport 0 --txport 1 --iterations 1 --duration 10
-folder=$(find . -name 'testresults*' | tail -n 1)
+sudo ./build/MoonGen rfc2544/master.lua --rxport 0 --txport 1 --iterations 1 --duration 10 --mlr 0.05
+# folder=$(find . -name 'testresults*' | tail -n 1)
